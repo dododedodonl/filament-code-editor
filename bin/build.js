397e9cc25643e35376e69821d868129b23788b98
Tom Janssen <dododedodonl@thor.edu>
2022-04-29 12:17:43 +0200

    minify file when building

--- bin/build.js
+++ bin/build.js
@@ -3,10 +3,11 @@ const shouldWatch = process.argv.includes('--watch');
 
 esbuild.build({
     define: {
-        'process.env.NODE_ENV': shouldWatch ? `'production'` : `'development'`,
+        'process.env.NODE_ENV': shouldWatch ? `'development'` : `'production'`,
     },
     entryPoints: ['resources/js/index.js'],
     outfile: 'dist/filament-tools.js',
     bundle: true,
+    minify: ! shouldWatch,
     watch: shouldWatch,
 }).catch(() => process.exit(1));
